e16a96efdeba2f9849e30b96c34cbeff15c0878f
Niv Zelber <nivznivz0@gamil.com>
2021-09-24 16:48:43 +0300

    remove default css and move global css from app.svelte to global.css

--- src/App.svelte
+++ src/App.svelte
@@ -23,24 +23,4 @@
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
-
-  :global(::-webkit-scrollbar) {
-    width: 2px;
-  }
-
-  :global(::-webkit-scrollbar-track) {
-    background: #f1f1f1;
-  }
-
-  :global(::-webkit-scrollbar-thumb) {
-    background: #888;
-  }
-
-  :global(::-webkit-scrollbar-thumb:hover) {
-    background: #555;
-  }
-
-  :global(body) {
-    overflow-y: hidden;
-  }
 </style>
